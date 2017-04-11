a0979c2c94a9b4d10e35334021062f67304a5d1b
ChuanxiZhang <chuanxiz@usc.edu>
2017-04-11 10:44:42 -0700

    V1.0001
    V1.0001

---
+++ Leetcode-C-Plus-Plus/206. Reverse Linked List .cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		ListNode* pre = NULL;
+		ListNode* next;
+		while (head) {
+			next = head->next;
+			head->next = pre;
+			pre = head;
+			head = next;
+		}
+		return pre;
+	}
+};
+
